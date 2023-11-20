ef13c11811d99ce7b9c05614e3f7cfb91f471d6f
paulo.zava <paulo.zava@olx.com>
2023-11-20 20:27:47 +0000

    fizzbuzz

--- lib/elixir_fizzbuzz.ex
+++ lib/elixir_fizzbuzz.ex
@@ -1,18 +1,18 @@
-defmodule ElixirFizzbuzz do
-  @moduledoc """
-  Documentation for `ElixirFizzbuzz`.
-  """
+defmodule Fizzbuzz do
+  def fizzbuzz(collection) do
+    Enum.map(collection, fn x ->
+      cond do
+        rem(x, 3) == 0 && rem(x, 5) != 0 -> "Fizz"
+        rem(x, 3) != 0 && rem(x, 5) == 0 -> "Buzz"
+        rem(x, 3) == 0 && rem(x, 5) == 0 -> "FizzBuzz"
+        rem(x, 3) != 0 && rem(x, 5) != 0 -> x
+      end
+    end)
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirFizzbuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def run() do
+    numbers = File.read!("numbers.txt")
+    numbers = String.trim(numbers) |> String.split(", ") |> Enum.map(fn x -> String.to_integer(x) end)
+    fizzbuzz(numbers)
   end
 end
