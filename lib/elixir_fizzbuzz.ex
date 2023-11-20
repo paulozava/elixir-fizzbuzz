f77c69e4bc6571b90ffaf0f6f75e4cd18b16e825
paulo.zava <paulo.zava@olx.com>
2023-11-20 21:40:36 +0000

    final

--- lib/elixir_fizzbuzz.ex
+++ lib/elixir_fizzbuzz.ex
@@ -1,20 +1,20 @@
 defmodule Fizzbuzz do
-  @spec fizzbuzz(any()) :: list()
   def fizzbuzz(collection) do
     Enum.map(collection, fn x ->
       cond do
-        rem(x, 3) == 0 && rem(x, 5) != 0 -> "Fizz"
-        rem(x, 3) != 0 && rem(x, 5) == 0 -> "Buzz"
-        rem(x, 3) == 0 && rem(x, 5) == 0 -> "FizzBuzz"
-        rem(x, 3) != 0 && rem(x, 5) != 0 -> x
+        rem(x, 15) == 0 -> "FizzBuzz"
+        rem(x, 3) == 0 -> "Fizz"
+        rem(x, 5) == 0 -> "Buzz"
+        true -> x
       end
     end)
   end
 
-  @spec run() :: list()
-  def run() do
-    numbers = File.read!("/Users/paulozava/Codes/elixir-fizzbuzz/lib/numbers.txt")
-    numbers = String.trim(numbers) |> String.split(", ") |> Enum.map(fn x -> String.to_integer(x) end)
-    fizzbuzz(numbers)
+  def run(file_path) do
+    File.read!(file_path)
+    |> String.trim()
+    |> String.split(", ")
+    |> Enum.map(&String.to_integer/1)
+    |> fizzbuzz()
   end
 end
--- test/elixir_fizzbuzz_test.exs
+++ test/elixir_fizzbuzz_test.exs
@@ -2,13 +2,51 @@ defmodule FizzbuzzTest do
   use ExUnit.Case
   doctest Fizzbuzz
 
-  test "run" do
-    assert Fizzbuzz.run() == [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14,
-    "FizzBuzz", 16, 17, "Fizz", 19, "Buzz", "Fizz", 22, 23, "Fizz", "Buzz", 26,
-    "Fizz", 28, 29, "FizzBuzz", 31, 32, "Fizz", 34, "Buzz"]
-  end
+  describe "run/1" do
+    test "when we provide a correct file, return the fizzbuzz list" do
+      expected = [
+        1,
+        2,
+        "Fizz",
+        4,
+        "Buzz",
+        "Fizz",
+        7,
+        8,
+        "Fizz",
+        "Buzz",
+        11,
+        "Fizz",
+        13,
+        14,
+        "FizzBuzz",
+        16,
+        17,
+        "Fizz",
+        19,
+        "Buzz",
+        "Fizz",
+        22,
+        23,
+        "Fizz",
+        "Buzz",
+        26,
+        "Fizz",
+        28,
+        29,
+        "FizzBuzz",
+        31,
+        32,
+        "Fizz",
+        34,
+        "Buzz"
+      ]
 
-  test "fizzbuzz" do
-    assert Fizzbuzz.fizzbuzz([1,2,3]) == [1,2,"Fizz"]
+      assert Fizzbuzz.run("numbers.txt") == expected
+    end
+
+    test "fizzbuzz" do
+      assert Fizzbuzz.fizzbuzz([1, 2, 3]) == [1, 2, "Fizz"]
+    end
   end
 end
